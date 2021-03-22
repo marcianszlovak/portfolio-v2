9e867c27291e5694b20f62657b6eb938b6f86a70
marci <marcianszlovak@gmail.com>
2021-03-22 19:09:05 +0100

    [chore] remove unnecessary folder for layouts

--- ClientApp/src/app/layouts/navigation/navigation.component.ts
+++ ClientApp/src/app/layouts/navigation/navigation.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, shareReplay } from 'rxjs/operators';
-import { LoaderService } from '../../../services/loader.service';
+import { LoaderService } from '../../services/loader.service';
 
 @Component({
   selector: 'app-navigation',
--- ClientApp/src/app/pages/homepage/homepage.module.ts
+++ ClientApp/src/app/pages/homepage/homepage.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { HomepageRoutingModule } from './homepage-routing.module';
-import { LayoutsModule } from '../../layouts/layouts/layouts.module';
+import { LayoutsModule } from '../../layouts/layouts.module';
 
 @NgModule({
   declarations: [HomepageComponent],
