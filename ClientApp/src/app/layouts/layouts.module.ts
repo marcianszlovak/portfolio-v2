f83523854671e2b0679a7aca767a7eabfb228320
marci <marcianszlovak@gmail.com>
2021-04-01 19:58:55 +0200

    [feat] create component for image bar

--- ClientApp/src/app/layouts/layouts.module.ts
+++ ClientApp/src/app/layouts/layouts.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { FooterComponent } from './footer/footer.component';
+import { ImageBarComponent } from './image-bar/image-bar.component';
 
 @NgModule({
-  declarations: [NavigationComponent, FooterComponent],
+  declarations: [NavigationComponent, FooterComponent, ImageBarComponent],
   imports: [CommonModule, RouterModule, FormsModule, MaterialModule],
-  exports: [NavigationComponent, FooterComponent],
+  exports: [NavigationComponent, FooterComponent, ImageBarComponent],
 })
 export class LayoutsModule {}
