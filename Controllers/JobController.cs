dbeed1b14126da2640b36ee4d87b7605af46864c
marci <marcianszlovak@gmail.com>
2021-04-07 15:58:57 +0200

    [feat] add pagination to job controller and get jobs filters (description, type, location)

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -15,29 +15,50 @@ namespace portfolio.Controllers
     {
         private readonly IJobService _jobService;
         private static readonly HttpClient Client = new HttpClient();
+        private readonly string _baseUrl;
 
 
         public JobController(IJobService jobService)
         {
             _jobService = jobService;
+            _baseUrl = "https://jobs.github.com/positions.json/";
         }
 
         [HttpGet]
         public async Task<IEnumerable<Job>> GetAllAsync()
         {
-            var streamTask = await Client.GetStreamAsync("https://jobs.github.com/positions.json");
+            var streamTask = await Client.GetStreamAsync($"{_baseUrl}");
             var jobs = await JsonSerializer.DeserializeAsync<List<Job>>(streamTask);
 
             return jobs;
         }
 
-        [HttpGet("{id}")]
-        public async Task<Job> GetJobById(string id)
-        {
-            var streamTask = await Client.GetStreamAsync($"https://jobs.github.com/positions/{id}.json");
-            var job = await JsonSerializer.DeserializeAsync<Job>(streamTask);
+        // [HttpGet("id/{id}")]
+        // public async Task<Job> GetJobById(string id)
+        // {
+        //     var streamTask = await Client.GetStreamAsync($"{_baseUrl}/positions/{id}.json");
+        //     var job = await JsonSerializer.DeserializeAsync<Job>(streamTask);
+        //
+        //     return job;
+        // }
 
-            return job;
+        [HttpGet("page/{pageNum}")]
+        public async Task<IEnumerable<Job>> GetJobsByPageNumber(string pageNum)
+        {
+            var streamTask = await Client.GetStreamAsync($"{_baseUrl}?page={pageNum}");
+            var jobs = await JsonSerializer.DeserializeAsync<List<Job>>(streamTask);
+
+            return jobs;
+        }
+
+        [HttpGet("description/{description}")]
+        public async Task<IEnumerable<Job>> GetJobsByDescription(string description, string type, string location, string pageNum)
+        {
+            var streamTask =
+                await Client.GetStreamAsync($"{_baseUrl}?description={description}&full_time={type}&location={location}&page={pageNum}"); 
+            var jobs = await JsonSerializer.DeserializeAsync<List<Job>>(streamTask);
+
+            return jobs;
         }
     }
 }
--- Domain/Models/Job.cs
+++ Domain/Models/Job.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace portfolio.Domain.Models
 {
